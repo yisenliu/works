664a6ab200460e2df22cf5a5d3b99efd51970f9b
Yisen <yisenliu@gmail.com>
2015-06-04 11:03:42 +0800

    JSHint

--- js/mousewheelEvents.js
+++ js/mousewheelEvents.js
@@ -1,35 +1,44 @@
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
 var keys = [37, 38, 39, 40];
+
 function preventDefault(e) {
-	e = e || window.event;// old IE support
-	if (e.preventDefault)
-		e.preventDefault();
-	e.returnValue = false;  
+  e = e || window.event; // old IE support
+  if (e.preventDefault)
+    e.preventDefault();
+  e.returnValue = false;
 }
+
 function keydown(e) {
-    for (var i = keys.length; i--;) {
-        if (e.keyCode === keys[i]) {
-            preventDefault(e);
-            return;
-        }
+  for (var i = keys.length; i--;) {
+    if (e.keyCode === keys[i]) {
+      preventDefault(e);
+      return;
     }
+  }
 }
+
 function wheel(e) {
   preventDefault(e);
 }
+
 function disable_scroll() {
-	if (window.addEventListener) {
-		window.addEventListener('DOMMouseScroll', wheel, false);
-	}
-	window.onmousewheel = document.onmousewheel = wheel;
-	document.onkeydown = keydown;
-	document.ontouchmove = function(e){ e.preventDefault(); }
+  if (window.addEventListener) {
+    window.addEventListener('DOMMouseScroll', wheel, false);
+  }
+  window.onmousewheel = document.onmousewheel = wheel;
+  document.onkeydown = keydown;
+  document.ontouchmove = function (e) {
+    e.preventDefault();
+  };
 }
+
 function enable_scroll() {
-    if (window.removeEventListener) {
-        window.removeEventListener('DOMMouseScroll', wheel, false);
-    }
-    window.onmousewheel = document.onmousewheel = document.onkeydown = null;  
-	document.ontouchmove = function(e){ return true; }
+  if (window.removeEventListener) {
+    window.removeEventListener('DOMMouseScroll', wheel, false);
+  }
+  window.onmousewheel = document.onmousewheel = document.onkeydown = null;
+  document.ontouchmove = function (e) {
+    return true;
+  };
 }
